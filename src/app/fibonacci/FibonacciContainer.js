16f65f8ff8e196f0f10665962dd955b85fea5966
christophergoot <christophergoot@gmail.com>
2018-09-10 14:49:53 -0700

    added "fibber" fake api response

--- src/app/fibonacci/FibonacciContainer.js
+++ src/app/fibonacci/FibonacciContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import FibonacciComponent from './FibonacciComponent';
+import { fetchFibonacciListSuccess } from '../../store';
 
 function mapStateToProps(state) {
 	return {
@@ -13,8 +14,22 @@ class FibonacciContainer extends Component {
 	handleNewRequest = event => {
 		event.preventDefault();
 		const digits = event.target[0].value;
-		alert(`new request to generate fibonacci to ${digits} digits`);
+		const newArr = this.Fibber(digits);
+		this.props.dispatch(fetchFibonacciListSuccess(newArr));
 	}
+
+	Fibber = digits => {
+		const fibArr = [0,1];
+		if (digits<3) return fibArr.slice(0,digits)
+		else {
+			for (let i=2; i<digits; i++) {
+				const nextNumber = fibArr[i-2] + fibArr[i-1];
+				fibArr.push(nextNumber);
+			};
+			return fibArr
+		}
+	};
+	
 
 	render() {
 		return (
--- src/store.js
+++ src/store.js
@@ -2,15 +2,28 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
 const initialState = ({
 	fibonacci: {
-		list: [0,1,1,2,3,4],
+		list: [],
 		digits: 0
 	}
 });
 
+export const FETCH_FIBONACCI_LIST_SUCCESS = 'FETCH_FIBONACCI_LIST_SUCCESS';
+export const fetchFibonacciListSuccess = list => ({
+	type: FETCH_FIBONACCI_LIST_SUCCESS,
+	list
+})
+
 const fibonacci = (state=initialState, action) => {
 	const { type } = action;
 	switch (type) {
-		
+		case FETCH_FIBONACCI_LIST_SUCCESS:
+			return {
+				...state,
+				fibonacci: {
+					list: action.list,
+					digits: action.list.length
+				}
+			}
 		
 		default:
 			return state;
